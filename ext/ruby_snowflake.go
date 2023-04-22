5c807114c2c984d4a24bbe7351ec0430cc601709
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2023-04-22 22:31:04 +0100

    Introduce return value options & additional duration logging
    Introduce options for the `select` method:
    - `map_fields: false`
    - `convert_dates: true`
    
    `map_fields` will return a `hash` where the keys are the column names
    (downcased and `to_sym`).
    
    `convert_dates` - will convert the dates coming from Go into Ruby dates.
    
    Update the `Go` code to return a few different types rather than just
    strings, namely:
    - `double` - for anything to do with numbers. The `go/sql` will convert
      any number into the largest possible type, which is `float64` which
      maps to the `double` C type.
    - `short` - for booleans, as there is no C _bool_ type.
    - strings for `time.Time` - explicitely calling out `time.Time` fields
      and formatting them to RFC3339, giving Ruby the option to call
      `DateTime.parse`
    - `string` - anything that does not fit into the above categories.
    
    The `Go` code for `next_row` will now return 2 arrays: one with the
    actual row values, and one with the row field types.

--- ext/ruby_snowflake.go
+++ ext/ruby_snowflake.go
@@ -7,11 +7,13 @@ import "C"
 import (
 	"database/sql"
 	"errors"
+	"fmt"
+	"io"
+	"time"
+	"unsafe"
+
 	gopointer "github.com/mattn/go-pointer"
 	sf "github.com/snowflakedb/gosnowflake"
-	"io"
-	"unsafe"
-	//  "fmt"
 )
 
 // Lazy coding: storing last error and connection as global vars bc don't want to figure out how to pkg and pass them
@@ -29,6 +31,7 @@ func LastError() *C.char {
 
 // @returns db pointer
 // ugh, ruby and go were disagreeing about the length of `int` so I had to be particular here and in the ffi
+//
 //export Connect
 func Connect(account *C.char, warehouse *C.char, database *C.char, schema *C.char,
 	user *C.char, password *C.char, role *C.char, port int64) unsafe.Pointer {
@@ -67,6 +70,7 @@ func Close(db_pointer unsafe.Pointer) {
 }
 
 // @return number of rows affected or -1 for error
+//
 //export Exec
 func Exec(db_pointer unsafe.Pointer, statement *C.char) int64 {
 	db := decodeDbPointer(db_pointer)
@@ -83,7 +87,9 @@ func Exec(db_pointer unsafe.Pointer, statement *C.char) int64 {
 func Fetch(db_pointer unsafe.Pointer, statement *C.char) unsafe.Pointer {
 	db := decodeDbPointer(db_pointer)
 	var rows *sql.Rows
+	t1 := time.Now()
 	rows, last_error = db.Query(C.GoString(statement))
+	fmt.Printf("Query duration: %s\n", time.Now().Sub(t1))
 	if rows != nil {
 		result := gopointer.Save(rows)
 		return result
@@ -93,6 +99,7 @@ func Fetch(db_pointer unsafe.Pointer, statement *C.char) unsafe.Pointer {
 }
 
 // @return column names[List<String>] for the given query.
+//
 //export QueryColumns
 func QueryColumns(rows_pointer unsafe.Pointer) **C.char {
 	rows := decodeRowsPointer(rows_pointer)
@@ -122,6 +129,7 @@ func QueryColumns(rows_pointer unsafe.Pointer) **C.char {
 }
 
 // @return column names[List<String>] for the given query.
+//
 //export QueryColumnCount
 func QueryColumnCount(rows_pointer unsafe.Pointer) int32 {
 	rows := decodeRowsPointer(rows_pointer)
@@ -134,8 +142,9 @@ func QueryColumnCount(rows_pointer unsafe.Pointer) int32 {
 }
 
 // NOTE: gc's the rows_pointer object on EOF and returns nil. LastError is set to EOF
+//
 //export NextRow
-func NextRow(rows_pointer unsafe.Pointer) **C.char {
+func NextRow(rows_pointer unsafe.Pointer) *unsafe.Pointer {
 	rows := decodeRowsPointer(rows_pointer)
 	if rows == nil {
 		return nil
@@ -145,35 +154,78 @@ func NextRow(rows_pointer unsafe.Pointer) **C.char {
 		columns, _ := rows.Columns()
 		rowLength := len(columns)
 
-		rawResult := make([][]byte, rowLength)
+		rawResult := make([]interface{}, rowLength)
 		rawData := make([]interface{}, rowLength)
-		for i, _ := range rawResult { // found in stackoverflow, fwiw
-			rawData[i] = &rawResult[i] // Put pointers to each string in the interface slice
+		for i := range rawResult {
+			rawData[i] = &rawResult[i]
 		}
 
 		// https://stackoverflow.com/questions/58866962/how-to-pass-an-array-of-strings-and-get-an-array-of-strings-in-ruby-using-go-sha
 		pointerSize := unsafe.Sizeof(rows_pointer)
 		// Allocate an array for the string pointers.
-		var out **C.char
-		out = (**C.char)(C.malloc(C.ulong(rowLength) * C.ulong(pointerSize)))
 
 		last_error = rows.Scan(rawData...)
 		if last_error != nil {
 			return nil
 		}
+
+		// Allocate an array for the string pointers.
+		var out *unsafe.Pointer
+		out = (*unsafe.Pointer)(C.malloc(C.ulong(rowLength) * C.ulong(pointerSize)))
 		pointer := out
+
+		var typesOut **C.char
+		typesOut = (**C.char)(C.malloc(C.ulong(rowLength) * C.ulong(pointerSize)))
+		typesPointer := typesOut
+
 		for _, raw := range rawResult {
-			// Find where to store the address of the next string.
 			// Copy each output string to a C string, and add it to the array.
 			// C.CString uses malloc to allocate memory.
+
+			//fix go pointer for for loop variable
+			raw := raw
 			if raw == nil {
 				*pointer = nil
+				*typesPointer = nil
 			} else {
-				*pointer = C.CString(string(raw))
+				rawType := "string"
+
+				switch v := raw.(type) {
+				case float64:
+					qq := C.double(v)
+					*pointer = unsafe.Pointer(&qq)
+					rawType = "double"
+				case bool:
+					qq := C.short(0)
+					if v {
+						qq = C.short(1)
+					}
+					*pointer = unsafe.Pointer(&qq)
+					rawType = "short"
+				case time.Time:
+					rawType = "time.Time"
+					*pointer = unsafe.Pointer(C.CString(v.Format(time.RFC3339)))
+				default:
+					*pointer = unsafe.Pointer(C.CString(fmt.Sprintf("%v", raw)))
+				}
+
+				t := C.CString(rawType)
+				*typesPointer = t
 			}
-			pointer = (**C.char)(unsafe.Pointer(uintptr(unsafe.Pointer(pointer)) + pointerSize))
+			pointer = (*unsafe.Pointer)(unsafe.Pointer(uintptr(unsafe.Pointer(pointer)) + pointerSize))
+			typesPointer = (**C.char)(unsafe.Pointer(uintptr(unsafe.Pointer(typesPointer)) + pointerSize))
 		}
-		return out
+
+		var finalOut *unsafe.Pointer
+		finalOut = (*unsafe.Pointer)(C.malloc(C.ulong(unsafe.Sizeof(out)) + C.ulong(unsafe.Sizeof(typesOut))))
+		finalPointer := finalOut
+		*finalPointer = unsafe.Pointer(out)
+		finalPointer = (*unsafe.Pointer)(unsafe.Pointer(uintptr(unsafe.Pointer(finalPointer)) + unsafe.Sizeof(pointerSize)))
+		*finalPointer = unsafe.Pointer(typesOut)
+		finalPointer = (*unsafe.Pointer)(unsafe.Pointer(uintptr(unsafe.Pointer(finalPointer)) + unsafe.Sizeof(pointerSize)))
+
+		return finalOut
+
 	} else if rows.Err() == io.EOF {
 		gopointer.Unref(rows_pointer) // free up for gc
 	}
--- lib/go_snowflake_client.rb
+++ lib/go_snowflake_client.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 $LOAD_PATH << File.dirname(__FILE__)
-require 'ruby_snowflake_client/version'
-require 'ffi'
+require "ruby_snowflake_client/version"
+require "ffi"
+require "benchmark"
 
-# Note: this library is not thread safe as it caches the last error
+# NOTE: this library is not thread safe as it caches the last error
 # The call pattern expectation is to call last_error after any call which may have gotten an error. If last_error is
 # `nil`, there was no error.
 module GoSnowflakeClient
@@ -17,10 +18,10 @@ module GoSnowflakeClient
     error
   end
 
-  # @param account[String] should include everything in the db url ahead of 'snowflakecomputing.com'
+  # @param account[String] should include everything in the db url ahead of "snowflakecomputing.com"
   # @param port[Integer]
   # @return query_object[Pointer] a pointer to use for subsequent calls not inspectable nor viewable by Ruby
-  def connect(account:"", warehouse: "", database: "", schema: "", user: "", password: "", role: "", port: 443)
+  def connect(account: "", warehouse: "", database: "", schema: "", user: "", password: "", role: "", port: 443)
     GoSnowflakeClientBinding.connect(account, warehouse, database, schema, user, password, role, port || 443)
   end
 
@@ -42,18 +43,36 @@ module GoSnowflakeClient
   # @param sql[String] a select query to run.
   # @return error_string
   # @yield List<String>
-  def select(db_pointer, sql, map_fields: false)
-    return 'db_pointer not initialized' unless db_pointer
-    return to_enum(__method__, db_pointer, sql, map_fields: map_fields) unless block_given?
+  def select(db_pointer, sql, map_fields: false, convert_dates: true)
+    return "db_pointer not initialized" unless db_pointer
+    unless block_given?
+      x = nil
+      time = Benchmark.measure {
+        x = to_enum(__method__, db_pointer, sql, map_fields: map_fields, convert_dates: convert_dates)
+      }
+      return x
+    end
 
-    query_pointer = fetch(db_pointer, sql)
+    query_pointer = nil
+    time = Benchmark.measure {
+      query_pointer = fetch(db_pointer, sql)
+    }
+
     return last_error if query_pointer.nil? || query_pointer == FFI::Pointer::NULL
 
     field_count = column_count(query_pointer)
-    field_names = column_names(query_pointer, field_count).map(&:to_sym)
+    field_names = column_names(query_pointer, field_count).map(&:downcase).map(&:to_sym)
+    total_time = 0
     loop do
-      row = get_next_row(query_pointer, field_count)
-      return last_error unless row
+      row = nil
+      time = Benchmark.measure {
+        row = get_next_row(query_pointer, field_count, convert_dates)
+      }
+      total_time += time.real
+      unless row
+        puts "Total Time for next_row: #{total_time}"
+        return last_error
+      end
 
       if map_fields
         yield field_names.zip(row).to_h
@@ -67,32 +86,52 @@ module GoSnowflakeClient
   # @param db_pointer[Pointer] the pointer which `connect` returned.
   # @param query[String] a select query to run.
   # @return query_object[Pointer] a pointer to use for subsequent calls not inspectable nor viewable by Ruby; however,
-  #   if it's `nil`, check `last_error`
+  #   if it"s `nil`, check `last_error`
   def fetch(db_pointer, query)
     GoSnowflakeClientBinding.fetch(db_pointer, query)
   end
 
   # @param query_object[Pointer] the pointer which `fetch` returned. Go will gc this object when the query is done; so,
-  #   don't expect to reference it after the call which returned `nil`
+  #   don"t expect to reference it after the call which returned `nil`
   # @param field_count[Integer] column count: it will seg fault if you provide a number greater than the actual number.
   #    Using code should use wrap this in something like
   #
   # @return [List<String>] the column values in order
-  def get_next_row(query_object, field_count)
-    raw_row = GoSnowflakeClientBinding.next_row(query_object)
+  def get_next_row(query_object, field_count, convert_dates)
+    next_row_ret = GoSnowflakeClientBinding.next_row(query_object)
+    return nil if next_row_ret == FFI::Pointer::NULL
+
+    raw_row, field_types_arr = next_row_ret.get_array_of_pointer(0, 2).map { _1 }
     return nil if raw_row.nil? || raw_row == FFI::Pointer::NULL
 
-    raw_row.get_array_of_pointer(0, field_count).map do |cstr|
+    field_types = field_types_arr.get_array_of_pointer(0, field_count).map do
+      _1.read_string unless _1 == FFI::Pointer::NULL
+    end
+
+    raw_row.get_array_of_pointer(0, field_count).map.with_index do |cstr, idx|
       if cstr == FFI::Pointer::NULL || cstr.nil?
         nil
       else
-        str = cstr.read_string
-        LibC.free(cstr)
-        str
+        val = case field_types[idx]
+              when "double"
+                cstr.read(:double)
+              when "short" # go binding returns short to encode a boolean
+                cstr.read(:short) == 1 # boolean
+              when "time.Time"
+                if convert_dates
+                  DateTime.parse(cstr.read_string)
+                else
+                  cstr.read_string
+                end
+              else
+                cstr.read_string
+              end
+        LibC.free(cstr) if field_types[idx] == "string"
       end
     end
   ensure
     LibC.free(raw_row) if raw_row
+    LibC.free(field_types_arr) if field_types_arr
   end
 
   # @param query_object[Pointer] the pointer which `fetch` returned.
@@ -131,15 +170,15 @@ module GoSnowflakeClient
 
     POINTER_SIZE = FFI.type_size(:pointer)
 
-    ffi_lib(File.expand_path('../ext/ruby_snowflake_client.so', __dir__))
-    attach_function(:last_error, 'LastError', [], :strptr)
+    ffi_lib(File.expand_path("../ext/ruby_snowflake_client.so", __dir__))
+    attach_function(:last_error, "LastError", [], :strptr)
     # ugh, `port` in gosnowflake is just :int; however, ruby - ffi -> go is passing 32bit int if I just decl :int.
-    attach_function(:connect, 'Connect', %i[string string string string string string string int64], :pointer)
-    attach_function(:close, 'Close', [:pointer], :void)
-    attach_function(:exec, 'Exec', %i[pointer string], :int64)
-    attach_function(:fetch, 'Fetch', %i[pointer string], :pointer)
-    attach_function(:next_row, 'NextRow', [:pointer], :pointer)
-    attach_function(:query_columns, 'QueryColumns', [:pointer], :pointer)
-    attach_function(:query_column_count, 'QueryColumnCount', [:pointer], :int32)
+    attach_function(:connect, "Connect", %i[string string string string string string string int64], :pointer)
+    attach_function(:close, "Close", [:pointer], :void)
+    attach_function(:exec, "Exec", %i[pointer string], :int64)
+    attach_function(:fetch, "Fetch", %i[pointer string], :pointer)
+    attach_function(:next_row, "NextRow", [:pointer], :pointer)
+    attach_function(:query_columns, "QueryColumns", [:pointer], :pointer)
+    attach_function(:query_column_count, "QueryColumnCount", [:pointer], :int32)
   end
 end
